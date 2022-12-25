2bd5e63ae8e02a65b3b5341e3f66fe98af19ba1b
InkoHX <me@inkohx.dev>
2022-12-25 10:58:57 +0900

    :wrench: add .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+}
